83031d2229f8073030f445f4579e9103250589ae
moogleKupo <tmaine@gmx.com>
2023-07-28 02:43:18 +0000

    add user registrstion form

--- express_server.js
+++ express_server.js
@@ -86,6 +86,15 @@ app.post("/logout", (req, res) => {
   res.redirect("/urls");
 });
 
+// New GET /register endpoint
+app.get("/register", (req, res) => {
+  const templateVars = {
+    username: req.cookies["username"],
+  };
+  res.render("register", templateVars);
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
